bdeaf6c217397899b73e480757081ee3a8bd468a
israad-visuals <101580272@georgebrown.ca>
2026-03-31 09:31:43 -0400

    backend's CORS configuration

--- src/main/java/com/subtrack/config/CorsConfig.java
+++ src/main/java/com/subtrack/config/CorsConfig.java
@@ -18,7 +18,7 @@ public class CorsConfig {
                 "http://localhost:3000",
                 "http://localhost:5500",
                 "http://127.0.0.1:5500",
-                "http://localhost:5173"
+                "http://localhost:5173", "https://systemtest.spiraml.com"
         ));
         config.setAllowedMethods(Arrays.asList(
                 "GET","POST","PUT","DELETE","PATCH","OPTIONS"));
